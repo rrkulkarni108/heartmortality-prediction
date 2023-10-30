7c35b4b5b3597b6a6d0918576d73778a2192cc38
Riana Guha <riana.guha@tamu.edu>
2023-10-30 11:42:33 -0500

    Trying imputation method

--- Missingdata.R
+++ Missingdata.R
@@ -1,6 +1,31 @@
 #Continuing from Preprocessing.R....
 ##MICE Package
 
+#Loading required package
+require(mice)
+require(VIM)
 
+#Loading data
+data <- read.csv("Heart_Disease_Mortality_Data_Among_US_Adults__35___by_State_Territory_and_County.csv", header= T)
+data_dropped <- data[,-c(5, 6 , 7 , 10, 11 , 12, 17)]
+data_tx <- data_dropped[data_dropped$LocationAbbr == "TX", ]
+
+#Check which columns have missing values
+colSums(is.na(data_tx))
+#1726 missing values in Data_Value column
+
+#percentage of missing data
+perc <- function(x) {sum(is.na(x))/length(x)*100}
+apply(data_tx, 2, perc)
+#37.60349% of the values in Data_Value are missing
+
+#Imputing
+impute <- mice(data_tx, m=5, meth ="pmm",seed = 123)
+summary(impute)
+
+imp_data_tx <- complete(impute,1)
+
+#Inspecting distribution of original data and imputed data
+densityplot(impute)
 
 
