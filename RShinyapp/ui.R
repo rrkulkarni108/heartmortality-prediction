e8df76ce3c7bf1f5e5fd3a5bec0c453fcb569317
Radhika Kulkarni <rrkulkarni@ucdavis.edu>
2023-11-28 17:26:09 -0600

    formatting

--- RShinyapp/ui.R
+++ RShinyapp/ui.R
@@ -11,15 +11,18 @@ ui <- fluidPage(
 
     sidebarLayout(
       sidebarPanel(
+        br(),
         selectInput("genderInput", "Select Gender:",
                     choices = c("Male", "Female"), selected = "All"),
+        br(),
         selectInput("raceInput", "Select Race/Ethnicity:",
                     choices = c("White", "Black", "Hispanic"), selected = "All"),
-	  selectInput("countyInput", "Select Texas County:",
-			  choices = 
-			    #choices = 
-			    unique(imp_gender_race_tx$County), selected = "All")
-      ),
+        br(),
+    	  selectInput("countyInput", "Select Texas County:",
+    			  choices = 
+    			    #choices = 
+    			    unique(imp_gender_race_tx$County), selected = "All")
+          ),
 
       mainPanel(
         leafletOutput("county_map")
